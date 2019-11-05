75721e2b0361168917c01cb13d96ab07ce36c880
Stephanie Simms <stephanie.simms@gmail.com>
2019-11-05 08:55:28 -0800

    added functions to submit form and add a todo

---
+++ todos_vanillaJS/app.js
@@ -0,0 +1,33 @@
+// add a todo by submitting form
+// mark a todo as completed w/ strikethrough when user clicks checkbox
+// delete a todo when user clicks trash can
+// clear the list of todos when user clicks Clear
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  loadEvents();
+});
+
+const loadEvents = () => {
+  document.querySelector('form').addEventListener('submit', submit);
+}
+
+const submit = (evt) => {
+  evt.preventDefault();
+  let newTask = document.querySelector('#newTask');
+  
+  if (newTask.value !== '') {
+    addTask(newTask.value);
+  }
+  newTask.value = '';
+}
+
+const addTask = (task) => {
+  let taskList = document.querySelector('ul');
+  let listItem = document.querySelector('li');
+  
+  listItem.innerHTML = `<span class="delete"><i class="fa fa-trash-alt"></i></span>
+          <input type="checkbox"><label>${task}</label>`;
+  taskList.appendChild(listItem);
+  document.querySelector('#tasks').style.display = 'block';
+}

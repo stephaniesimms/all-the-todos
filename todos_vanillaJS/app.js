7dfca907b1cddb076f85c344bf439a94b2b108ab
Stephanie Simms <stephanie.simms@gmail.com>
2019-11-05 10:16:15 -0800

    added functions to deleteOrMark and delete a task

--- todos_vanillaJS/app.js
+++ todos_vanillaJS/app.js
@@ -1,18 +1,20 @@
-// add a todo by submitting form
 // mark a todo as completed w/ strikethrough when user clicks checkbox
 // delete a todo when user clicks trash can
-// clear the list of todos when user clicks Clear
+
 
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM fully loaded and parsed');
   loadEvents();
 });
 
+// load all event listeners
 const loadEvents = () => {
   document.querySelector('form').addEventListener('submit', submitForm);
   document.querySelector('#clearList').addEventListener('click', clearList);
+  document.querySelector('ul').addEventListener('click', deleteOrMark);
 }
 
+// submit the form with new tasks input
 const submitForm = (evt) => {
   evt.preventDefault();
   let newTask = document.querySelector('#newTask');
@@ -23,18 +25,39 @@ const submitForm = (evt) => {
   newTask.value = '';
 }
 
+// add the new task to the task list
 const addTask = (task) => {
   let taskList = document.querySelector('ul');
   let listItem = document.createElement('li');
   
-  listItem.innerHTML = `<span class="delete"><i class="fa fa-trash-alt"></i></span>
+  listItem.innerHTML = `<i class="delete fa fa-trash-alt"></i>
           <input type="checkbox"><label>${task}</label>`;
   taskList.appendChild(listItem);
   document.querySelector('#tasks').style.display = 'block';
 }
 
-const clearList = (evt) => {
+// clear/reset the list of tasks and hide it
+const clearList = () => {
   let listToClear = document.querySelector('ul');
   listToClear.innerHTML = '';
   document.querySelector('#tasks').style.display = 'none';
+}
+
+
+const deleteOrMark = (evt) => {
+  if (evt.target.tagName.toLowerCase() === 'i') {
+    deleteTask(evt.target.parentNode);
+  } else {
+    markTask(evt.target.nextSibling);
+  }
+}
+
+const deleteTask = (taskNode) => {
+  taskNode.remove();
+  // TODO: add check to see if that's the last item
+  // if so hide list
+}
+
+const markTask = (taskNode) => {
+  console.log('mark task event')
 }
